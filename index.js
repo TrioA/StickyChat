2c90bc7dfa1ad0c3037292459757a45469131dd0
TrioA <153422978+TrioA@users.noreply.github.com>
2025-01-12 20:44:05 +0530

    Socket.IO Basics
    Implemented basic Socket.IO in the code.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+
 const app = express();
-const port = process.env.PORT || 3000;
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('msg', (msg) => {
+    console.log('Message: ' + msg);
+    io.emit('msg', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 app.listen(port, () => {
--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
-alert("ITS WORKING!!!");
+const socket = io();
+const input = document.querySelector("#note");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", ev => {
+    socket.emit('msg', input.value);
+    input.value = '';
+});
+
+socket.on('msg', (msg) => {
+    alert(msg);
+});
